7535584da91457cf8ac786e9e9918dd06c218e4f
Carter <speers.carter@gmail.com>
2025-06-28 17:09:40 -0400

    refactor: modularized main.cpp

---
+++ util.cpp
@@ -0,0 +1,73 @@
+//
+// Created by carter on 6/28/25.
+//
+
+#include "util.h"
+
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+
+
+std::string key_code_to_string(const int code) {
+
+  auto it = keymap.find(code);
+  if (it != keymap.end()) {
+    return it->second;
+  }
+  return "UNKNOWN_KEY_" + std::to_string(code);
+}
+
+std::string find_keyboard_device() {
+  std::ifstream devices_file("/proc/bus/input/devices");
+  if (!devices_file.is_open()) {
+    throw std::runtime_error(
+        "Failed to open /proc/bus/input/devices. Are you root?");
+  }
+
+  std::string line;
+  std::string current_device_handler;
+  bool found_keyboard = false;
+
+  while (std::getline(devices_file, line)) {
+    if (line.empty()) {
+      // Reset on blank lines which separate device entries
+      current_device_handler = "";
+      found_keyboard = false;
+      continue;
+    }
+
+    std::stringstream ss(line);
+    std::string key;
+    ss >> key;
+
+    if (key == "H:") {
+      // Handlers line
+      std::string handlers;
+      std::getline(ss, handlers); // Read the rest of the line
+      std::stringstream handler_ss(handlers);
+      std::string handler_part;
+      while (handler_ss >> handler_part) {
+        if (handler_part.rfind("event", 0) == 0) {
+          current_device_handler = "/dev/input/" + handler_part;
+        }
+      }
+    } else if (key == "B:") {
+      std::string pair;
+      ss >> pair;
+      std::stringstream pair_ss(pair);
+
+      if (pair == "EV=120013") {
+        found_keyboard = true;
+      }
+    }
+
+    // If we found a keyboard and have a handler for it, we are done
+    if (found_keyboard && !current_device_handler.empty()) {
+      return current_device_handler;
+    }
+  }
+
+  throw std::runtime_error(
+      "Could not find a keyboard device. Check /proc/bus/input/devices.");
+}
---
+++ util.h
@@ -0,0 +1,117 @@
+//
+// Created by carter on 6/28/25.
+//
+
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <string>
+#include <map>
+
+#include <linux/input-event-codes.h>
+
+static const std::map<int, std::string> keymap = {
+      {KEY_RESERVED, "RESERVED"},
+      {KEY_ESC, "ESC"},
+      {KEY_1, "1"},
+      {KEY_2, "2"},
+      {KEY_3, "3"},
+      {KEY_4, "4"},
+      {KEY_5, "5"},
+      {KEY_6, "6"},
+      {KEY_7, "7"},
+      {KEY_8, "8"},
+      {KEY_9, "9"},
+      {KEY_0, "0"},
+      {KEY_MINUS, "-"},
+      {KEY_EQUAL, "="},
+      {KEY_BACKSPACE, "BACKSPACE"},
+      {KEY_TAB, "TAB"},
+      {KEY_Q, "Q"},
+      {KEY_W, "W"},
+      {KEY_E, "E"},
+      {KEY_R, "R"},
+      {KEY_T, "T"},
+      {KEY_Y, "Y"},
+      {KEY_U, "U"},
+      {KEY_I, "I"},
+      {KEY_O, "O"},
+      {KEY_P, "P"},
+      {KEY_LEFTBRACE, "["},
+      {KEY_RIGHTBRACE, "]"},
+      {KEY_ENTER, "ENTER"},
+      {KEY_LEFTCTRL, "LEFTCTRL"},
+      {KEY_A, "A"},
+      {KEY_S, "S"},
+      {KEY_D, "D"},
+      {KEY_F, "F"},
+      {KEY_G, "G"},
+      {KEY_H, "H"},
+      {KEY_J, "J"},
+      {KEY_K, "K"},
+      {KEY_L, "L"},
+      {KEY_SEMICOLON, ";"},
+      {KEY_APOSTROPHE, "'"},
+      {KEY_GRAVE, "`"},
+      {KEY_LEFTSHIFT, "LEFTSHIFT"},
+      {KEY_BACKSLASH, "\\"},
+      {KEY_Z, "Z"},
+      {KEY_X, "X"},
+      {KEY_C, "C"},
+      {KEY_V, "V"},
+      {KEY_B, "B"},
+      {KEY_N, "N"},
+      {KEY_M, "M"},
+      {KEY_COMMA, ","},
+      {KEY_DOT, "."},
+      {KEY_SLASH, "/"},
+      {KEY_RIGHTSHIFT, "RIGHTSHIFT"},
+      {KEY_KPASTERISK, "KPASTERISK"},
+      {KEY_LEFTALT, "LEFTALT"},
+      {KEY_SPACE, "SPACE"},
+      {KEY_CAPSLOCK, "CAPSLOCK"},
+      {KEY_F1, "F1"},
+      {KEY_F2, "F2"},
+      {KEY_F3, "F3"},
+      {KEY_F4, "F4"},
+      {KEY_F5, "F5"},
+      {KEY_F6, "F6"},
+      {KEY_F7, "F7"},
+      {KEY_F8, "F8"},
+      {KEY_F9, "F9"},
+      {KEY_F10, "F10"},
+      {KEY_NUMLOCK, "NUMLOCK"},
+      {KEY_SCROLLLOCK, "SCROLLLOCK"},
+      {KEY_KP7, "KP7"},
+      {KEY_KP8, "KP8"},
+      {KEY_KP9, "KP9"},
+      {KEY_KPMINUS, "KPMINUS"},
+      {KEY_KP4, "KP4"},
+      {KEY_KP5, "KP5"},
+      {KEY_KP6, "KP6"},
+      {KEY_KPPLUS, "KPPLUS"},
+      {KEY_KP1, "KP1"},
+      {KEY_KP2, "KP2"},
+      {KEY_KP3, "KP3"},
+      {KEY_KP0, "KP0"},
+      {KEY_KPDOT, "KPDOT"},
+      {KEY_UP, "UP"},
+      {KEY_DOWN, "DOWN"},
+      {KEY_LEFT, "LEFT"},
+      {KEY_RIGHT, "RIGHT"},
+      {KEY_PAGEUP, "PAGEUP"},
+      {KEY_PAGEDOWN, "PAGEDOWN"},
+      {KEY_HOME, "HOME"},
+      {KEY_END, "END"},
+      {KEY_INSERT, "INSERT"},
+      {KEY_DELETE, "DELETE"}};
+
+// Function to map key codes to their string representations.
+// A more comprehensive map can be found in
+// /usr/include/linux/input-event-codes.h
+std::string key_code_to_string(int code);
+
+
+std::string find_keyboard_device();
+
+#endif //UTIL_H
